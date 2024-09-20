d97f2e1ba870d7148bd7f23770ad13e78e4b7faf
Matthew Parker <maddparker@gmail.com>
2024-09-20 12:58:52 -0400

    Update DeckOfCards.cs
    removed unused vars

--- CodeLab2-BlackJack/Assets/Scripts/Core/DeckOfCards.cs
+++ CodeLab2-BlackJack/Assets/Scripts/Core/DeckOfCards.cs
@@ -5,8 +5,6 @@ using System.Collections.Generic;
 
 public class DeckOfCards : MonoBehaviour {
 	
-	public Text cardNumUI;
-	public Image cardImageUI;
 	public Sprite[] cardSuits;
 
 	public class Card{
