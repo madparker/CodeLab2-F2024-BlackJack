da822c7235a9d5e36fc1cd9bd3ad0c76555556cb
Nengkuan Chen <chen_n1@denison.edu>
2024-09-26 17:24:01 -0400

    Remove DEBUG_DECK define symbol

--- CodeLab2-BlackJack/Assets/Scripts/Editor/OpenPersistentDataPath.cs
+++ CodeLab2-BlackJack/Assets/Scripts/Editor/OpenPersistentDataPath.cs
@@ -1,7 +1,22 @@
-namespace DefaultNamespace
+
+
+using UnityEditor;
+using UnityEngine;
+
+public class OpenPersistentDataPath
 {
-    public class OpenPersistentDataPath
+    [MenuItem("Tools/Open Persistent Data Path")]
+    public static void Open()
     {
+        //if mac os
+#if UNITY_EDITOR_OSX
+        EditorUtility.RevealInFinder(Application.persistentDataPath);
+#endif
+        
+#if UNITY_EDITOR_WIN
+        System.Diagnostics.Process.Start("explorer.exe", Application.persistentDataPath.Replace("/", "\\"));
+#endif
+        
         
     }
 }
