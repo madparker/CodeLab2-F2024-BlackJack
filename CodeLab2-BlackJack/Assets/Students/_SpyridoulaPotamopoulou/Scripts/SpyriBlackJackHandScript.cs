7c386743b068fe8135f881322465476b01f468d9
ESP-YRI <sap637@nyu.edu>
2024-09-26 16:40:45 -0400

    Finished Fixing Game Mechanics
    Finished the autowin calculator and added a script which accounts for variability in the value of aces in the case of near-busts. Also, added a better background for the game.

--- CodeLab2-BlackJack/Assets/Students/_SpyridoulaPotamopoulou/Scripts/SpyriBlackJackHandScript.cs
+++ CodeLab2-BlackJack/Assets/Students/_SpyridoulaPotamopoulou/Scripts/SpyriBlackJackHandScript.cs
@@ -4,27 +4,52 @@ using UnityEngine;
 
 public class SpyriBlackJackHandScript : BlackJackHand
 {
-    private int initialScore = 0;
-
-    protected override void ShowValue()
+    private bool playerHasHit = false;
+    
+    protected override void SetupHand()
     {
-        base.ShowValue();
+        //sets up the player's hand
+        base.SetupHand();
+        
+        //checks if the value of the player's hand is 21 at the time of this check (the start of the game)
+        //since the HitMe function called within SetupHand calls the ShowValue function at its end, you do not
+        //need to call ShowValue here to get the value of the player's hand- it has already been calculated!
         if (handVals == 21)
         {
-            GameObject.Find("BlackJackManager").GetComponent<BlackJackManager>().PlayerWin();
+            //if yes, initiates the game over condition indicating the player has won and gotten a BlackJack
+            GameObject.Find("BlackJackManager").GetComponent<BlackJackManager>().BlackJack();
         }
 
+        //prints the handVals to the console integer for testing purposes
         Debug.Log(handVals);
     }
-
+    
+    
+    //my original solution to the autowin, which is more complicated than it needed to be but which still works
+    
     /*
-    void AutoWinCheck()
+    protected override void ShowValue()
     {
-        initialScore = GetHandValue();
-        if (initialScore == 21)
+        //runs the original showValue function to get the value of the player's hand
+        base.ShowValue();
+        
+        //if the value of the player's hand is 21 and they have not yet hit (i.e. it is the start of the game)
+        if (handVals == 21 && playerHasHit == false)
         {
-            GameObject.Find("BlackJackManager").GetComponent<BlackJackManager>().PlayerWin();
+            Debug.Log(playerHasHit);
+            //initiate the game over condition indicating the player has won and gotten an automatic BlackJack
+            GameObject.Find("BlackJackManager").GetComponent<BlackJackManager>().BlackJack();
         }
-    }
-    */
+
+        //prints the handVals to the console integer for testing purposes
+        Debug.Log(handVals);
+    } 
+    
+    //checks if the player has pressed the hit button 
+    //must be put into the Hit button in the inspector to work
+    public void HitCheck()
+    {
+        playerHasHit = true;
+    }*/
+
 }
