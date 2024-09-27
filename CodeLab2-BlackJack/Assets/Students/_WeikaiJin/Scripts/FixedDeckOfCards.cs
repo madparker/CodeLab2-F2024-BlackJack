41ee38363c61713b9c3ca8c6103da301395af1d7
Fiiction <Kai_Jin_1998@163.com>
2024-09-27 11:52:47 -0400

    Fixed Deck of Cards.

--- CodeLab2-BlackJack/Assets/Students/_WeikaiJin/Scripts/FixedDeckOfCards.cs
+++ CodeLab2-BlackJack/Assets/Students/_WeikaiJin/Scripts/FixedDeckOfCards.cs
@@ -6,6 +6,12 @@ namespace Weikai
 {
     public class FixedDeckOfCards : DeckOfCards
     {
+        protected override bool IsValidDeck()
+        {
+            if(deck == null) return false;
+            return deck.Count >= 20;
+        }
+
         protected override void AddCardsToDeck()
         {
             for (int i = 0; i < 4; i++)
