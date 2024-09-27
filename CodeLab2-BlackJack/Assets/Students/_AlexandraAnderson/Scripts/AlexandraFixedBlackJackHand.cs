cc3c9552c5201ad9faa02c4f706a4acdded2082b
alexandravkanderson <ava8270@nyu.edu>
2024-09-27 12:40:23 -0400

    currently a bit broken
    :)

--- CodeLab2-BlackJack/Assets/Students/_AlexandraAnderson/Scripts/AlexandraFixedBlackJackHand.cs
+++ CodeLab2-BlackJack/Assets/Students/_AlexandraAnderson/Scripts/AlexandraFixedBlackJackHand.cs
@@ -5,18 +5,35 @@ using UnityEngine;
 namespace AlexandraAnderson
 {
     
-    public class AlexandraFixedBlackJackHand : MonoBehaviour
+    public class AlexandraFixedBlackJackHand : BlackJackHand
     {
-        // Start is called before the first frame update
-        void Start()
+        public void ResetHand()
         {
-            
+            SetupHand();
         }
-    
-        // Update is called once per frame
-        void Update()
+        protected override void SetupHand()
         {
+          
+            for ( int i =0; i < transform.childCount; i++) {Destroy(transform.GetChild(i).gameObject);
+                
+            }
+
+            //if (tester == 0)
+            //{
+            //    Debug.Log("transform.ChildCount: " + transform.childCount);
+            //}
             
+            
+            base.SetupHand();
+            
+            // Natural Black Jack: If player starts with 21, player automatically wins
+            if (handVals == 21)
+            {
+                Debug.Log("Natural Black Jack");
+                
+                GameObject.Find("BlackJackManager").GetComponent<BlackJackManager>().BlackJack();
+                
+            }
         }
     }
     
