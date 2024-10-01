3087cc9a494e14f443be9f468cefb0c321f810ea
isabell2018 <157531053+isabell2018@users.noreply.github.com>
2024-10-01 19:34:27 -0400

    Add Modified BlackJack

--- CodeLab2-BlackJack/Assets/Students/_IsabelLiang/Scripts/FixedDealerHand.cs
+++ CodeLab2-BlackJack/Assets/Students/_IsabelLiang/Scripts/FixedDealerHand.cs
@@ -6,8 +6,13 @@ namespace IsabelLiang
 {
     public class FixedDealerHand : DealerHand
     {
+
+        protected override bool DealStay(int handVal){
+            //if the value is over or equal to 17 the dealer will stay and not draw another card
+            return handVal >= 17;
+        }
         
-    
+        
     }
 }
 
